088102df823017f2be77ec455a21dd6b5b03fc6a
Arsalan Siddiqui <siddiq11@myumanitoba.ca>
2023-05-29 22:52:11 +0000

    Devtask login view handler

--- app/src/main/java/BookWorm/View/UserLogin_ViewHandler.java
+++ app/src/main/java/BookWorm/View/UserLogin_ViewHandler.java
@@ -1,40 +1,74 @@
 package BookWorm.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
 
 import com.honda.bookworm.R;
 
 public class UserLogin_ViewHandler extends AppCompatActivity {
 
-    private EditText userNameInput;
-    private EditText passwordInput;
-
-    private Button loginButton;
-    private TextView linkToSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-        //get reference to layout components
-        userNameInput = findViewById(R.id.userlogin_username_input);
-        passwordInput = findViewById(R.id.userlogin_password_input);
-        loginButton = findViewById(R.id.userlogin_login_button);
-        linkToSignup = findViewById(R.id.userlogin_signup_link);
-
         stylizeLink();
+
+        listenSignupLinkText();
+        listenClickOnLoginButton();
     }
 
-    private void stylizeLink(){
+    private void stylizeLink() {
+        TextView linkToSignup = findViewById(R.id.userlogin_signup_link);
         linkToSignup.setPaintFlags(linkToSignup.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
+
+
+    private void listenSignupLinkText(){
+        TextView linkToSignup = findViewById(R.id.userlogin_signup_link);
+        linkToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserSignup_ViewHandler.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void listenClickOnLoginButton() {
+        Button loginButton = findViewById(R.id.userlogin_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proceedToLogin();
+            }
+        });
+    }
+
+    private void proceedToLogin(){
+        EditText usernameInput = (EditText) findViewById(R.id.userlogin_username_input);
+        EditText passwordInput = (EditText) findViewById(R.id.userlogin_password_input);
+
+        String msg = String.format("Username: %s \nPassword: %s", usernameInput.getText(),
+                passwordInput.getText());
+
+
+        //toast makes the popup notification on the screen
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+
 
 
 
