375ff2ae74cad1d435b249823b199897536bc7c0
Fidelio Ciandy <ciandyf@myumanitoba.ca>
2023-05-30 02:27:50 +0000

    Create userStub DB

---
+++ app/src/main/java/BookWorm/Data/Stubs/UserPersistenceStub.java
@@ -0,0 +1,71 @@
+package BookWorm.Data.Stubs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import BookWorm.Object.User;
+
+import BookWorm.Data.UserPersistence;
+
+public class UserPersistenceStub implements UserPersistence {
+    private List<User> users;
+    public UserPersistenceStub(){
+        this.users = new ArrayList<>();
+
+        users.add(new User("John", "Doe", "johndoe", "password1"));
+        users.add(new User("Jane", "Smith", "janesmith", "password2"));
+        users.add(new User("John", "Wick", "johnwick", "password3"));
+        users.add(new User("Joe", "Mama", "joemama", "password4"));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return Collections.unmodifiableList(users);
+    }
+
+    @Override
+    public User getUserByUsername(String currentUsername) {
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getUsername().equalsIgnoreCase(currentUsername)){
+                return users.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public User addUser(User currentUser) {
+        boolean duplicate = users.contains(currentUser);
+
+        //check duplicate
+        if(!duplicate){
+            users.add(currentUser);
+            return currentUser;
+        }
+        return null;
+    }
+
+    @Override
+    public User updateUser(User currentUser, User updateUser) {
+        int index = users.indexOf(currentUser);
+
+        //check exist
+        if(index >= 0){
+            users.set(index,updateUser);
+            return updateUser;
+        }
+        return null;
+    }
+
+    @Override
+    public User removeUser(User currentUser) {
+        int index = users.indexOf(currentUser);
+
+        //check exist
+        if(index >= 0){
+            users.remove(index);
+            return currentUser;
+        }
+        return null;
+    }
+}
