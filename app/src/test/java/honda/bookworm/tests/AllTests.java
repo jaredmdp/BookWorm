26b733b7c5526aae487ca4d98db4aedbe2257f5b
Jared Mandap <jaredmandap@gmail.com>
2023-06-05 18:17:34 -0500

    Refactor AllTests

--- app/src/test/java/honda/bookworm/tests/AllTests.java
+++ app/src/test/java/honda/bookworm/tests/AllTests.java
@@ -3,23 +3,10 @@ package honda.bookworm.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import honda.bookworm.tests.Business.AccessBooksTest;
-import honda.bookworm.tests.Data.BookPersistenceStubTest;
-import honda.bookworm.tests.Data.UserPersistenceStubTest;
-import honda.bookworm.tests.Object.BookTest;
-import honda.bookworm.tests.Object.UserTest;
-import honda.bookworm.tests.Object.AuthorTest;
-import honda.bookworm.tests.Business.AccessUsersTest;
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-                BookTest.class,
-                UserTest.class,
-                AuthorTest.class,
-                UserPersistenceStubTest.class,
-                BookPersistenceStubTest.class,
-                AccessBooksTest.class,
-                AccessUsersTest.class
+                AllIntegrationTests.class,
+                AllUnitTests.class
 })
 
 public class AllTests {
