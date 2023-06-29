767af198ec183eba0a5460499ba8a9a8eed854dd
Fidelio Ciandy <ciandyf@myumanitoba.ca>
2023-06-29 22:21:03 +0000

    Resolve "DevTask: Make add book stub layout"

---
+++ app/src/main/java/honda/bookworm/View/AddBook_ViewHandler.java
@@ -0,0 +1,29 @@
+package honda.bookworm.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.honda.bookworm.R;
+
+public class AddBook_ViewHandler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book_view_handler);
+    }
+
+    public void isAvailableToPurchaseClicked(View view) {
+    }
+
+    public void isAddImageButtonClicked(View view) {
+    }
+
+    public void isCancelButtonClicked(View view) {
+    }
+
+    public void isAddBookButtonClicked(View view) {
+    }
+}
