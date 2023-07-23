da54d64364d74a36b2bc905128ad74bc0d4a17b5
Jared Mandap <jaredmandap@gmail.com>
2023-07-22 20:13:15 -0500

    add features page + link with navbar

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -283,18 +283,7 @@ const NAV_ITEMS: Array<NavItem> = [
   },
   {
     label: "Features",
-    children: [
-      {
-        label: "Job Board",
-        subLabel: "Find your dream design job",
-        href: "#",
-      },
-      {
-        label: "Freelance Projects",
-        subLabel: "An exclusive list for contract work",
-        href: "#",
-      },
-    ],
+    href: "/FeaturePage",
   },
   {
     label: "Vision Statement",
