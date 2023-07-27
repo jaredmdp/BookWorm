6fee1b8abe3e6fc17c343480fe5ce157a70504f7
Jared Mandap <jaredmandap@gmail.com>
2023-07-27 18:24:29 -0500

    make video mobile responsive

--- website/app/Video/page.tsx
+++ website/app/Video/page.tsx
@@ -1,27 +1,51 @@
 "use client";
 import React from "react";
-import { Center, Heading } from "@chakra-ui/react";
-import YouTube from "react-youtube";
+import {
+  Box,
+  Center,
+  Heading,
+  AspectRatio,
+  useBreakpointValue,
+  Spacer,
+} from "@chakra-ui/react";
 
 const Video = () => {
   const videoId = "AN9OX2ZJ5y0";
 
-  const playerOptions = {
-    height: "480",
-    width: "800",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
+  const aspectRatio = useBreakpointValue({ base: 5 / 4, md: 16 / 9 });
 
   return (
     <>
-      <Heading as="h1" size="2xl" textAlign="center" mb="4" p="4">
+      <Heading as="h1" size="2xl" textAlign="center" my="4" p="4">
         Watch our presentation!
       </Heading>
-      <Center p="15">
-        <YouTube videoId={videoId} opts={playerOptions} />
+      <Center>
+        <Box
+          width="100%"
+          maxW={{ base: "100%", md: "800px" }}
+          boxShadow="xl"
+          borderRadius="md"
+          overflow="hidden"
+          position="relative"
+        >
+          <AspectRatio ratio={aspectRatio}>
+            <Box
+              as="iframe"
+              title="YouTube video player"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              frameBorder="0"
+              allowFullScreen
+              position="absolute"
+              top="0"
+              left="0"
+              width="100%"
+              height="100%"
+              objectFit="cover"
+            />
+          </AspectRatio>
+        </Box>
       </Center>
+      <Spacer height="70px" />
     </>
   );
 };
